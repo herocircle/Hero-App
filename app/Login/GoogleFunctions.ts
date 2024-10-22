f0a7e66a52aa8ef063296e11b6f31818f406e425
botross <botrosazizbotros@gmail.com>
2024-10-22 11:28:33 +0700

    Google Auth

--- app/Login/GoogleFunctions.ts
+++ app/Login/GoogleFunctions.ts
@@ -1,7 +1,7 @@
 
-const webClientId = ""
-const IosClientId = ''
-const AndroidClientId = ''
+const webClientId = "962812642379-fta2vdv858g417ke76jns14380hvinki.apps.googleusercontent.com"
+const IosClientId = '962812642379-t2l4kesogmp4f92qqjooab88vguutoj9.apps.googleusercontent.com'
+const AndroidClientId = '962812642379-qr9lsjgo3tao2c5n6q0fvcrcb2tm3fen.apps.googleusercontent.com'
 
 
 
